8e3a073cd2647fc4112f601fe29f5484cbf7ea90
claycarpenter <claycarpenter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-11 00:14:00 +0000

    Adding version 0.1.
    git-svn-id: https://plugins.svn.wordpress.org/getgit/trunk@679453 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ Plugin Name: GetGit
+ Plugin URI: http://flightlessflyer.pinguinotech.com/github-wordpress-plugin
+ Description: Embeds content stored in a public GitHub repository.
+ Version: 0.1
+ Author: Clay Carpenter
+ */
+
+/*
+ * This file contains the core plugin code, especially the
+ * GitHubRepoContentRetriever class.
+ */
+require_once ('php/github_repo_content_retriever.php');
+
+/*
+ * This file contains the options page manangement code (registration,
+ * definition, creation).
+ */
+require_once ('php/options_page_manager.php');
+
+/*
+ * Instantiate the core plugin class. The sole argument will configure the
+ * plugin with the correct base URL for this plugin. That knowledge allows the
+ * plugin to accurately point to (JS, CSS) resource files.
+ */
+new GitHubRepoContentRetriever( plugin_dir_url( __FILE__ ) );
+
+if ( is_admin( ) ) {
+	new OptionsPageManager( );
+}
+?>
